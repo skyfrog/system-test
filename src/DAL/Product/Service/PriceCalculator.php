25a9f68c4f5cdb49b3a109e1da14229c0db2262e
Vadim Manikhin <vadim.manikhin@gmail.com>
2023-07-16 18:26:12 +0400

    price calculator service.

--- src/DAL/Product/Service/PriceCalculator.php
+++ src/DAL/Product/Service/PriceCalculator.php
@@ -2,13 +2,46 @@
 
 namespace App\DAL\Product\Service;
 
+use App\Domain\Coupon\Model\ProcessCouponModel;
+use App\Domain\Coupon\Service\ProcessCouponInterface;
 use App\Domain\Product\Model\CalcPriceModel;
 use App\Domain\Product\Service\PriceCalculatorInterface;
+use App\Domain\VatNumber\Service\PriceModificatorBasedOnVatNumberInterface;
+use App\Exception\ValidationError;
+use App\Repository\ProductRepository;
 
 class PriceCalculator implements PriceCalculatorInterface
 {
+    public function __construct(
+        private readonly ProductRepository $productRepository,
+        private readonly ProcessCouponInterface $processCoupon,
+        private readonly PriceModificatorBasedOnVatNumberInterface $priceModificatorBasedOnVatNumber
+    )
+    {
+    }
+
+    /**
+     * @throws ValidationError
+     */
     public function calculate(CalcPriceModel $model): string
     {
-        return "1.01";
+        if (!($product = $this->productRepository->find($model->getProductId()))) {
+            throw new ValidationError('Product not found');
+        }
+
+        $price = $product->getPrice();
+
+        if ($model->getCouponCode()) {
+            $price = $this->processCoupon->processCoupon(new ProcessCouponModel(
+                $price,
+                $model->getCouponCode()
+            ));
+        }
+
+        if ($model->getTaxNumber()) {
+            $price = $this->priceModificatorBasedOnVatNumber->modify($price, $model->getTaxNumber());
+        }
+
+        return $price;
     }
 }
